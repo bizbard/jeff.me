d256f95e9a527d83e2dbfec6297aa7e970e1eb6a
bizbard <2180893013@qq.com>
2023-02-28 14:55:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { property: 'og:title', content: 'Chuanqi Jiao' },
+    // { property: 'og:image', content: 'https://antfu.me/og-icon.png' },
+    { property: 'og:image', content: 'https://antfu.me/og-icon.png' },
+    { name: 'description', content: 'Anthony Fu\'s Portfolio' },
+    // { name: 'twitter:card', content: 'summary' },
+    // { name: 'twitter:creator', content: '@antfu7' },
+  ],
+})
+</script>
+
+<template>
+  <NavBar />
+  <main class="px-7 py-10">
+    <router-view />
+    <Footer />
+  </main>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
+    <span class="text-sm">Inspired by © Anthony Fu</span>
+    <div class="flex-auto" />
+  </div>
+</template>
